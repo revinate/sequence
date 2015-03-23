b0dd4d272aa570a7385969182c801f02daea4c44
Jason Dent <jason@revinate.com>
2015-03-23 14:58:30 +0100

    REV-9949 add FnGenTests

--- tests/FnGenTest.php
+++ tests/FnGenTest.php
@@ -14,4 +14,25 @@ class FnGenTest extends PHPUnit_Framework_TestCase {
         $this->assertTrue($fn("hello") === "hello");
     }
 
+    public function testPluck() {
+        $fn = FnGen::fnPluck('value');
+        $doc = array('value'=>42, 'question'=>'What is the meaning of life?');
+
+        $this->assertTrue($fn($doc) == $doc['value']);
+        $this->assertTrue($fn(array('no-value'=>0)) === null);
+    }
+
+    public function testFnCounter() {
+        $fn = FnGen::fnCounter(0);
+        $this->assertTrue($fn() == 0);
+        $this->assertTrue($fn() == 1);
+        $this->assertTrue($fn() == 2);
+        $this->assertTrue($fn() == 3);
+
+        $fn = FnGen::fnCounter(10);
+        $this->assertTrue($fn() == 10);
+        $this->assertTrue($fn() == 11);
+        $this->assertTrue($fn() == 12);
+        $this->assertTrue($fn() == 13);
+    }
 }
