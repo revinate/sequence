35f2612edb765f5fc180ad9ada6ea2e15dfd17ee
Jason Dent <jason@revinate.com>
2015-08-30 20:49:28 +0200

    Add some tutorial code and data

---
+++ Tutorial/SampleDataLoader.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: jasondent
+ * Date: 30/08/15
+ * Time: 18:13
+ */
+class SampleDataLoader {
+
+    /**
+     * @param string $filename
+     * @param bool   $asArray
+     * @return mixed
+     */
+    public static function dataLoader($filename, $asArray) {
+        $json = file_get_contents($filename);
+        return json_decode($json, $asArray);
+    }
+
+    /**
+     * @param bool|true $asArray
+     * @return array
+     */
+    public static function getDonutSample($asArray = true) {
+        // Data From https://adobe.github.io/Spry/samples/data_region/JSONDataSetSample.html Example 5
+        $filename = __DIR__.'/Data/example_data_donuts.json';
+        return self::dataLoader($filename, $asArray);
+    }
+
+    /**
+     * @param bool|true $asArray
+     * @return mixed
+     */
+    public static function getLibraryThingComSample($asArray = true) {
+        // http://www.librarything.com/api_getdata.php?userid=timspalding&showstructure=1&max=10&showCollections=1&showTags=1&booksort=title_REV&responseType=json
+        $filename = __DIR__.'/Data/example_data_donuts.json';
+        return self::dataLoader($filename, $asArray);
+    }
+
+    /**
+     * @param bool|true $asArray
+     * @return mixed
+     */
+    public static function getEmployees($asArray = true) {
+        $filename = __DIR__.'/Data/employees.json';
+        return self::dataLoader($filename, $asArray);
+    }
+}
---
+++ Tutorial/tutorial1.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jasondent
+ * Date: 30/08/15
+ * Time: 18:06
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Revinate\Sequence\Sequence;
+use Revinate\Sequence\fn as fn;
+
+require_once 'SampleDataLoader.php';
+
+function employeeExample1() {
+    $employeeData = SampleDataLoader::getEmployees(true);
+
+    $employees = $employeeData['employees'];
+
+    // Assignment -- get a list of employees names
+
+    // Get a list of employees foreach
+    $names = array();
+    foreach ($employees as $employee) {
+        $names[] = $employee['firstName'];
+    }
+    print_r($names);
+
+    // Using a sequence
+    $names2 = Sequence::make($employees)
+        ->pluck('firstName')
+        ->to_a();
+
+    print_r($names2);
+}
+
+
+employeeExample1();
+
